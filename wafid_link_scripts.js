95aadf2980f7586b1593f2a4fb5f213e16dfc4a2
linkwafid-max <linkwafid@gmail.com>
2025-11-14 01:39:58 +0600

    Add system availability message for users
    Added a message indicating system availability.

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,3 +1,4 @@
 Username: test.admin432                      Total balance: 128.37
                       Welcome to our system
+  You will be able to work again when the system opens tomorrow.
 No link
