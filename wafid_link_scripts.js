12c69f8d8b05deb8599962048886d31adb43a426
linkwafid-max <linkwafid@gmail.com>
2025-11-17 01:01:57 +0600

    Update system status message with time

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,5 +1,5 @@
 Username: test.admin432                      Total balance: 128.37
-                          System Offline
+Time: 01:00 AM            System Offline
 1. https://wafid.com/appointment/RkGA5312wNkj4D6/pay/ (Validation Time  5 minutes)
 2. https://wafid.com/appointment/ZfAh6903wNkMn44/pay/ (Validation Time  5 minutes)
 3. https://wafid.com/appointment/McGA555JNknb4L/pay/ (Validation Time  5 minutes)
