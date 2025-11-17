e09e732de6a0d1eedbe53d872eafacfd5b4d4a85
linkwafid-max <linkwafid@gmail.com>
2025-11-17 23:05:41 +0600

    Update balance and system status in wafid_link_scripts.js
    Updated total balance and system status in the script.

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,5 +1,3 @@
-Username: test.admin432                      Total balance: 56.37
-Time: 01:00 AM            System Offline
-1. https://wafid.com/appointment/RkGA5312wNkj4D6/pay/ (Validation Time  5 minutes)
-2. https://wafid.com/appointment/ZfAh6903wNkMn44/pay/ (Validation Time  5 minutes)
-3. https://wafid.com/appointment/McGA555JNknb4L/pay/ (Validation Time  5 minutes)
+Username: test.admin432                      Total balance: 128.37
+System Online      The system will only be online for 20 minutes from (11:03 to 11:25).
+No link
