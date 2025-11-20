0fdf11320c39080f95706515caeeb9022393d193
linkwafid-max <linkwafid@gmail.com>
2025-11-21 01:25:12 +0600

    Change total balance in wafid_link_scripts.js
    Updated total balance from 128.37 to 58.37.

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,4 +1,4 @@
-Username: test.admin432                      Total balance: 128.37
+Username: test.admin432                      Total balance: 58.37
                           System Offline 
 Time: 01:13 AM
 https://wafid.com/appointment/pPavnB9AAPGeNrL/pay/ (Validation time only 5 minutes)
