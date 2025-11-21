48f04b194b34698f1614d47520eb503a019ef96b
linkwafid-max <linkwafid@gmail.com>
2025-11-21 22:40:50 +0600

    Update wafid_link_scripts.js

--- wafid_link_scripts.js
+++ wafid_link_scripts.js
@@ -1,6 +1,3 @@
 Username: test.admin432                      Total balance: 58.37
-                          System Offline 
-Time: 01:13 AM
-https://wafid.com/appointment/pPavnB9AAPGeNrL/pay/ (Validation time only 5 minutes)
-https://wafid.com/appointment/POE95bkPP8JeQaA/pay/ (Validation time only 5 minutes)
-https://wafid.com/appointment/VP9p5LWppKj4dzm/pay/ (Validation time only 5 minutes)
+                          System Online 
+No link
